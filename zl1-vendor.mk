eab3c298d116a29acbb050cba51b1c3757e4ae17
Ícaro Hoff <icarohoff@gmail.com>
2017-06-23 11:17:03 -0300

    zl1: sync audio configuration and ACDB data with zeusis
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- zl1-vendor.mk
+++ zl1-vendor.mk
@@ -103,6 +103,8 @@ PRODUCT_COPY_FILES += \
     vendor/leeco/zl1/proprietary/etc/ad_calib_le_zl1.cfg:system/etc/ad_calib_le_zl1.cfg \
     vendor/leeco/zl1/proprietary/etc/qxdm/1.cfg:system/etc/qxdm/1.cfg \
     vendor/leeco/zl1/proprietary/etc/qxdm/gps_log.cfg:system/etc/qxdm/gps_log.cfg \
+    vendor/leeco/zl1/proprietary/etc/surround_sound_3mic/surround_sound_rec_5.1.cfg:system/etc/surround_sound_3mic/surround_sound_rec_5.1.cfg \
+    vendor/leeco/zl1/proprietary/etc/surround_sound_3mic/surround_sound_rec_AZ.cfg:system/etc/surround_sound_3mic/surround_sound_rec_AZ.cfg \
     vendor/leeco/zl1/proprietary/lib/egl/egl.cfg:system/lib/egl/egl.cfg
 
 # Database
