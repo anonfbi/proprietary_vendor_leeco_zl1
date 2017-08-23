79aa0ab8e61b64a1cf121aa5ece70d1a0cd2982c
Ícaro Hoff <icarohoff@gmail.com>
2017-08-23 13:58:10 -0300

    zl1: properly initialize blobs repository using the extractor
    We were using a non-fashion way of getting the blobs, which was
    manually, this is bad and not effective.
    
    So, step ahead and convert to OTA extraction based method using our new
    global scripts in AOSPA (based off LineageOS ones).
    
    Command:
    ./extract-files.sh -p $repo/system_dump
    
    Change-Id: I69f64551bddae18e27a7df71f936fec63ce8c5e2
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

---
+++ extract-files.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+#
+# Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+set -e
+
+DEVICE=zl1
+VENDOR=leeco
+
+MY_DIR="${BASH_SOURCE%/*}"
+if [[ ! -d "$MY_DIR" ]]; then MY_DIR="$PWD"; fi
+
+ROOT="$MY_DIR"/../../..
+
+HELPER="$ROOT"/vendor/blobscript/extract_utils.sh
+if [ ! -f "$HELPER" ]; then
+    echo "Unable to find helper script at $HELPER"
+    exit 1
+fi
+. "$HELPER"
+
+clean_vendor=false
+
+while [ "$1" != "" ]; do
+    case $1 in
+        -p | --path )           shift
+                                SRC=$1
+                                ;;
+        -s | --section )        shift
+                                SECTION=$1
+                                ;;
+        -c | --clean-vendor )   clean_vendor=true
+                                ;;
+    esac
+    shift
+done
+
+if [ -z "$SRC" ]; then
+    SRC=adb
+fi
+
+setup_vendor "$DEVICE" "$VENDOR" "$ROOT" false $clean_vendor
+
+extract "$MY_DIR"/proprietary-files.txt "$SRC" "$SECTION"
+
+"$MY_DIR"/setup-makefiles.sh
---
+++ zl1-vendor.mk
@@ -0,0 +1,62 @@
+# Copyright (C) 2017 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+PRODUCT_COPY_FILES += \
+    vendor/leeco/zl1/proprietary/bin/ATFWD-daemon:system/bin/ATFWD-daemon \
+    vendor/leeco/zl1/proprietary/bin/adsprpcd:system/bin/adsprpcd \
+    vendor/leeco/zl1/proprietary/bin/btnvtool:system/bin/btnvtool \
+    vendor/leeco/zl1/proprietary/bin/cnss-daemon:system/bin/cnss-daemon \
+    vendor/leeco/zl1/proprietary/bin/cnss_diag:system/bin/cnss_diag \
+    vendor/leeco/zl1/proprietary/bin/energy-awareness:system/bin/energy-awareness \
+    vendor/leeco/zl1/proprietary/bin/gx_fpd:system/bin/gx_fpd \
+    vendor/leeco/zl1/proprietary/bin/hci_qcomm_init:system/bin/hci_qcomm_init \
+    vendor/leeco/zl1/proprietary/bin/ipacm-diag:system/bin/ipacm-diag \
+    vendor/leeco/zl1/proprietary/bin/irsc_util:system/bin/irsc_util \
+    vendor/leeco/zl1/proprietary/bin/mbn_daemon:system/bin/mbn_daemon \
+    vendor/leeco/zl1/proprietary/bin/mbn_daemon_test:system/bin/mbn_daemon_test \
+    vendor/leeco/zl1/proprietary/bin/mm-qcamera-daemon:system/bin/mm-qcamera-daemon \
+    vendor/leeco/zl1/proprietary/bin/pm-proxy:system/bin/pm-proxy \
+    vendor/leeco/zl1/proprietary/bin/pm-service:system/bin/pm-service \
+    vendor/leeco/zl1/proprietary/bin/qmuxd:system/bin/qmuxd \
+    vendor/leeco/zl1/proprietary/bin/qseecomd:system/bin/qseecomd \
+    vendor/leeco/zl1/proprietary/bin/radish:system/bin/radish \
+    vendor/leeco/zl1/proprietary/bin/rmt_storage:system/bin/rmt_storage \
+    vendor/leeco/zl1/proprietary/bin/sensors.qcom:system/bin/sensors.qcom \
+    vendor/leeco/zl1/proprietary/bin/ssr_diag:system/bin/ssr_diag \
+    vendor/leeco/zl1/proprietary/bin/ssr_setup:system/bin/ssr_setup \
+    vendor/leeco/zl1/proprietary/bin/tftp_server:system/bin/tftp_server \
+    vendor/leeco/zl1/proprietary/bin/time_daemon:system/bin/time_daemon \
+    vendor/leeco/zl1/proprietary/bin/wcnss_filter:system/bin/wcnss_filter \
+    vendor/leeco/zl1/proprietary/bin/yep_qmi_server:system/bin/yep_qmi_server \
+    vendor/leeco/zl1/proprietary/etc/camera/imx298_chromatix.xml:system/etc/camera/imx298_chromatix.xml \
+    vendor/leeco/zl1/proprietary/etc/camera/imx298_ofilm_ak_chromatix.xml:system/etc/camera/imx298_ofilm_ak_chromatix.xml \
+    vendor/leeco/zl1/proprietary/etc/camera/imx298_ofilm_chromatix.xml:system/etc/camera/imx298_ofilm_chromatix.xml \
+    vendor/leeco/zl1/proprietary/etc/camera/imx298_truly_ak_chromatix.xml:system/etc/camera/imx298_truly_ak_chromatix.xml \
+    vendor/leeco/zl1/proprietary/etc/camera/msm8996_camera.xml:system/etc/camera/msm8996_camera.xml \
+    vendor/leeco/zl1/proprietary/etc/camera/ov8865_chromatix.xml:system/etc/camera/ov8865_chromatix.xml \
+    vendor/leeco/zl1/proprietary/etc/camera/ov8865_ofilm_chromatix.xml:system/etc/camera/ov8865_ofilm_chromatix.xml \
+    vendor/leeco/zl1/proprietary/etc/camera/ov8865_ofilm_plus_chromatix.xml:system/etc/camera/ov8865_ofilm_plus_chromatix.xml \
+    vendor/leeco/zl1/proprietary/etc/camera/ov8865_sunny_chromatix.xml:system/etc/camera/ov8865_sunny_chromatix.xml \
+    vendor/leeco/zl1/proprietary/etc/firmware/nvm_tlv.bin:system/etc/firmware/nvm_tlv.bin \
+    vendor/leeco/zl1/proprietary/etc/firmware/nvm_tlv_1.3.bin:system/etc/firmware/nvm_tlv_1.3.bin \
+    vendor/leeco/zl1/proprietary/etc/firmware/nvm_tlv_2.1.bin:system/etc/firmware/nvm_tlv_2.1.bin \
+    vendor/leeco/zl1/proprietary/etc/firmware/nvm_tlv_3.0.bin:system/etc/firmware/nvm_tlv_3.0.bin \
+    vendor/leeco/zl1/proprietary/etc/firmware/nvm_tlv_3.2.bin:system/etc/firmware/nvm_tlv_3.2.bin \
+    vendor/leeco/zl1/proprietary/etc/firmware/rampatch_tlv.img:system/etc/firmware/rampatch_tlv.img \
+    vendor/leeco/zl1/proprietary/etc/firmware/rampatch_tlv_1.3.tlv:system/etc/firmware/rampatch_tlv_1.3.tlv \
+    vendor/leeco/zl1/proprietary/etc/firmware/rampatch_tlv_2.1.tlv:system/etc/firmware/rampatch_tlv_2.1.tlv \
+    vendor/leeco/zl1/proprietary/etc/firmware/rampatch_tlv_3.0.tlv:system/etc/firmware/rampatch_tlv_3.0.tlv \
+    vendor/leeco/zl1/proprietary/etc/firmware/rampatch_tlv_3.2.tlv:system/etc/firmware/rampatch_tlv_3.2.tlv \
+    vendor/leeco/zl1/proprietary/etc/qdcm_calib_data_mdss_dsi_ft8716_1080p_video.xml:system/etc/qdcm_calib_data_mdss_dsi_ft8716_1080p_video.xml \
+    vendor/leeco/zl1/proprietary/etc/qdcm_calib_data_mdss_dsi_s6d6fa1_1080p_video.xml:system/etc/qdcm_calib_data_mdss_dsi_s6d6fa1_1080p_video.xml
