ad11b8620e4b94715c603dcbc96f42ff8f800f67
Ícaro Hoff <icarohoff@gmail.com>
2017-08-27 15:57:47 -0300

    zl1: add --renamed and --help arguments in sha1sum script
    The new --renamed argument works the same as --added since git doesn't
    show us the previous file name to be replaced (with sed or awk) so we
    need to remove the old ones manually and re-run this script.
    
    Change-Id: I768cff8d856fd98aeda2c2c30463a1f372132ded
    Signed-off-by: Ícaro Hoff <icarohoff@gmail.com>

--- sha1sum-files.sh
+++ sha1sum-files.sh
@@ -11,16 +11,25 @@ function information {
     echo "This is useful for devices that use another source"
     echo "for most of their blobs that are not the OEM ones."
     echo ""
-    echo "Warning: Use ~ git add ~ to index the files before"
+    echo "Warning: Use [git add -A] to index the files before"
     echo "running this script."
     echo ""
     echo "Usage:"
+    echo ""
     echo "./sha1sum-files.sh --added"
     echo "   This will pin all the new files in the git index."
     echo ""
     echo "./sha1sum-files.sh --modified"
-    echo "   This will update the SHA for the modified files.."
-    echo
+    echo "   This will update the SHA for the modified files."
+    echo ""
+    echo "./sha1sum-files.sh --renamed"
+    echo "   This will pin the modified files if you have"
+    echo "   moved or renamed them."
+    echo ""
+    echo "./sha1sum-files.sh --help"
+    echo "   Shows this information just for you."
+    echo ""
+    echo "Disclaimer: This script is copyleft, take whatwever you need."
 }
 
 while [ "$1" != "" ]; do
@@ -31,14 +40,36 @@ while [ "$1" != "" ]; do
         -m | --modified )    filter="M --cached"
                              MODE="MOD"
                              ;;
+        -r | --renamed )     filter="R --cached"
+                             MODE="REN"
+                             ;;
+        -h | --help )        HELP="yes"
+                             ;;
     esac
     shift
 done
 
-if [ -z "$filter" ]; then
+if [ ! -z $HELP ]; then
     information
     exit 1
 fi
+
+if [ -z "$filter" ]; then
+    echo "Something went wrong, try running:"
+    echo "./sha1sum-files.sh -h"
+    exit 0
+fi
+
+# For renamed files, we need to remove the deprecated lines
+# after it was renamed.
+if [ $MODE = "REN" ]; then
+    BREN=$(git diff -M --cached --name-status | awk '{print $2}')
+    for BRENN in $BREN; do
+        BNAME=$(echo $BRENN | sed 's/proprietary//' | sed 's/^[-\/]*//')
+        NUM=$(grep -n "$BNAME" $PROPRIETARY | sed 's/^\([0-9]\+\):.*$/\1/')
+        sed -i "${NUM}d" $PROPRIETARY
+    done
+fi;
 
 # Query every changed file accounting to the respective filter.
 FILES=$(git diff --name-only --diff-filter=$filter)
@@ -46,7 +77,7 @@ for FILE in $FILES; do
     SHA=$(sha1sum $FILE | awk '{print $1}')
     NAME=$(echo $FILE | sed 's/proprietary//' | sed 's/^[-\/]*//')
     # Just add the new files.
-    if [ $MODE = "ADD" ]; then
+    if [ $MODE = "ADD" ] || [ $MODE = "REN" ]; then
         echo "$NAME|$SHA" >> $PROPRIETARY
     elif [ $MODE = "MOD" ]; then
         PINNED=$(cat $PROPRIETARY | grep $NAME | grep "|")
